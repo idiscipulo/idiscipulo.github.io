9d5ec240a078df0342c084219a55f16ab76781fd
idiscipulo <ikdiscipulo@gmail.com>
2021-06-04 11:01:29 -0700

    three factions

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function top_new_list_click() {
     document.getElementById("faction").style.display = "block";
 }
 
-function faction_hypermaw_servants_click() {
+function faction_hivespawn_drones_click() {
     state = "list_main"
 
     list_main_header = "=!= HYPERMAW SERVANTS =!=";
@@ -23,6 +23,17 @@ function faction_walrus_collective_click() {
     state = "list_main"
 
     list_main_header = "=!= WALRUS COLLECTIVE =!=";
+    document.getElementById("list_main_header").innerText = list_main_header
+
+    document.getElementById("top").style.display = "none";
+    document.getElementById("faction").style.display = "none";
+    document.getElementById("list_main").style.display = "block";
+}
+
+function faction_servants_of_luna_click() {
+    state = "list_main"
+
+    list_main_header = "=!= SERVANTS OF LUNA =!=";
     document.getElementById("list_main_header").innerText = list_main_header
 
     document.getElementById("top").style.display = "none";
