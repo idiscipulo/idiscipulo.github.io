db76b3d8549ebb624547f21a279194b79105b6c1
idiscipulo <ikdiscipulo@gmail.com>
2021-06-03 17:51:02 -0700

    more list functionality

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
+var list_main_header = ""
+var state = "top" //top, faction, list_main
+
 function top_new_list_click() {
+    state = "faction"
+
     document.getElementById("top").style.display = "none";
     document.getElementById("faction").style.display = "block";
 }
 
-function faction_hypermaw_drifters_click() {
+function faction_hypermaw_servants_click() {
+    state = "list_main"
+
+    list_main_header = "=!= HYPERMAW SERVANTS =!=";
+    document.getElementById("list_main_header").innerText = list_main_header
+
+    document.getElementById("top").style.display = "none";
+    document.getElementById("faction").style.display = "none";
+    document.getElementById("list_main").style.display = "block";
 }
 
 function faction_walrus_collective_click() {
+    state = "list_main"
+
+    list_main_header = "=!= WALRUS COLLECTIVE =!=";
+    document.getElementById("list_main_header").innerText = list_main_header
+
+    document.getElementById("top").style.display = "none";
+    document.getElementById("faction").style.display = "none";
+    document.getElementById("list_main").style.display = "block";
 }
 
-function faction_back_click() {
-    document.getElementById("top").style.display = "block";
-    document.getElementById("faction").style.display = "none";  
+function back_click() {
+    if (state == "faction") {
+        state = "top"
+
+        document.getElementById("top").style.display = "block";
+        document.getElementById("faction").style.display = "none";
+        document.getElementById("list_main").style.display = "none"; 
+    }else if (state == "list_main") {
+        state = "faction"
+
+        document.getElementById("top").style.display = "none";
+        document.getElementById("faction").style.display = "block";
+        document.getElementById("list_main").style.display = "none"; 
+    } 
 }
